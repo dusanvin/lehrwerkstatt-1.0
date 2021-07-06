0524172460551494248e666f81d924c43136d138
lumlop <vincent.dusanek@gmail.com>
2021-07-06 10:43:04 +0200

    Test Git Hub

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -75,7 +75,7 @@
                                                     </div>
 
                                                     <div class="ml-4">
-                                                        <div class="text-sm leading-5 font-medium text-gray-900">John Doe
+                                                        <div class="text-sm leading-5 font-medium text-gray-900">John Doe Granger
                                                         </div>
                                                         <div class="text-sm leading-5 text-gray-500">john@example.com</div>
                                                     </div>

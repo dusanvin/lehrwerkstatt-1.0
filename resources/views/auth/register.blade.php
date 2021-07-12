45e1491ead97b3b2c49733cf1b2667d3b5afd96c
sn2016 <sn2016@users.noreply.github.com>
2021-07-12 15:00:32 +0200

    register.blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -142,6 +142,16 @@
 										name="password"
 										
 									/>
+									@error('password')
+
+										<div class="text-red-500 mt-2 text-sm">
+											{{ $message }}
+										</div>
+
+									@enderror
+
+									
+
 
 								</div>
 

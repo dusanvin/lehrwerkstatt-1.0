2350b8b2840ae100dae104e7931a429769b3f343
sn2016 <sn2016@users.noreply.github.com>
2021-09-01 21:32:20 +0200

    Update user.blade.php

--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -143,6 +143,8 @@
                         </dd>
 
                       </div>
+                          
+                      </div>
 
                       <!-- <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
 

44279104932ca516e9e27fc5ab55537fd2ddcca5
sn2016 <sn2016@users.noreply.github.com>
2021-09-12 10:21:26 +0200

    scrollbar

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -21,7 +21,7 @@
 @include('layouts.header')
 
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 main-img">
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-y-scroll sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>

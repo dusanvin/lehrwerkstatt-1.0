6cbe7bc5aa824cee204dc68666d31420c73ca858
lumlop <vincent.dusanek@gmail.com>
2021-09-15 10:07:00 +0200

    Update needs.blade.php

--- resources/views/needs.blade.php
+++ resources/views/needs.blade.php
@@ -400,9 +400,9 @@
 
                                                     <button type="submit" class="py-2 text-purple-300 hover:text-purple-500 text-xs flex focus:outline-none ml-4">
 
-                                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                        <!--<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                                             <path fill-rule="evenodd" d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z" clip-rule="evenodd" />
-                                                        </svg><!--<span class="mx-1 mt-1">Anfragen</span>-->
+                                                        </svg></span>--><span class="">Anfragen
 
                                                     </button>
 

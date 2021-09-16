4c6f40a05c1b1ccb90d30e6fe2dcf377e307e87e
lumlop <vincent.dusanek@gmail.com>
2021-09-16 09:34:26 +0200

    Responsive-Anpassungen

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -4,9 +4,8 @@
 	    background-position: center center;
 	    background-repeat: no-repeat;
 	    background-size: cover;
-	    min-height: 120vh;
-	    width: 100%;
 	}
+
 	body span,label,a,button,div,p,input,label {
         font-family: 'Segoe UI' !important;
     }
@@ -20,8 +19,8 @@
 
 @include('layouts.header')
 
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 main-img">
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md sm:rounded-lg">
+<div class="flex flex-col items-center justify-center main-img flex-col py-6 sm:py-12 min-h-full sm:min-h-screen">
+    <div class="w-full sm:max-w-md my-12 px-6 py-12 bg-white shadow-md sm:rounded-lg">
         {{ $slot }}
     </div>
 </div>

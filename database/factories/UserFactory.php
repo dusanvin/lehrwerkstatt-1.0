31bc4dac2b3bad1155ddcce89100d4213f6c2c99
sn2016 <sn2016@users.noreply.github.com>
2021-09-18 10:55:56 +0200

    user seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
-    use HasFactory;
     /**
      * The name of the factory's corresponding model.
      *
@@ -24,11 +23,12 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
+            'vorname' => $this->faker->firstName,
+            'nachname' => $this->faker->lastName,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            // 'remember_token' => Str::random(10),
         ];
     }
 

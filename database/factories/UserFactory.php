76f6f3b2da4907fbaeff409e3d9f8402c96c78fc
sn2016 <sn2016@users.noreply.github.com>
2021-09-18 13:03:51 +0200

    neue user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,6 +6,9 @@ use App\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
+use Illuminate\Support\Facades\DB;
+
+
 class UserFactory extends Factory
 {
     /**
@@ -30,6 +33,13 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             // 'remember_token' => Str::random(10),
         ];
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (User $user) {
+            DB::insert('insert into model_has_roles (role_id, model_type, model_id) values (?, ?, ?)', [$this->faker->randomElement([4, 5]), 'App\Models\User', $user->id]);
+        });
     }
 
     /**

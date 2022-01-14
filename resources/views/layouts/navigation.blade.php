0a3bb6327a57befc6fb4ec78b0df240e2a6128f8
lumlop <vincent.dusanek@gmail.com>
2022-01-14 12:25:25 +0100

    Navigation End

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -78,7 +78,7 @@
 
 				  <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
 
-				</svg><span class="navigation-element font-semibold"> {{ Auth::user()->vorname }} {{ Auth::user()->nachname }}
+				</svg><span class="navigation-element font-semibold px-2"> {{ Auth::user()->vorname }} {{ Auth::user()->nachname }}
 				<!-- 
 
 				$firstName = (Auth::user()->vorname)[0];

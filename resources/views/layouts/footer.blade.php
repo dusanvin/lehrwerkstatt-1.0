00b71dbad9f8dfd71ce0ab277b8b62cfa0a78d5f
lumlop <vincent.dusanek@gmail.com>
2022-01-17 11:58:49 +0100

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -32,7 +32,7 @@
 
                         <p class="text-gray-500 transition-colors duration-200 font-normal mt-2">
 
-                            Das <em>digi:match</em> ist eine Plattform des DigiLLab der Uni Augsburg, um Unterstützende im Rahmen des Projekts <em>Augsburger DaZ-Buddys</em> zu finden.
+                            Das <em>digi:match</em> ist eine Plattform des DigiLLab der Uni Augsburg, um Paare im Rahmen des Projekts <em>Augsburger DaZ-Buddys</em> zu bilden.
 
                         </p>
 

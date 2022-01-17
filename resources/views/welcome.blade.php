6f45555f0e85eab555fc7b8efaa3d1a936d6ec1f
lumlop <vincent.dusanek@gmail.com>
2022-01-17 12:29:59 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -153,8 +153,9 @@
             </div>
 
             <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+                <!-- Old image https://images.pexels.com/photos/5212320/pexels-photo-5212320.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260 -->
 
-                <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.pexels.com/photos/5212320/pexels-photo-5212320.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
+                <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.pexels.com/photos/8423367/pexels-photo-8423367.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="">
 
             </div>
 

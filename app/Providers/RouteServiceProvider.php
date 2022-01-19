3e4e9cccf9bb66a7e65eb117326dad125992dcad
lumlop <vincent.dusanek@gmail.com>
2022-01-19 14:41:07 +0100

    User Edit als Home und Dashboard raus

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     /* public const HOME = '/dashboard'; */
-    public const HOME = '/dashboard';
+    public const HOME = '/profile/edit';
 
     /**
      * The controller namespace for the application.
--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__.'/auth.php';
 require __DIR__.'/api.php';

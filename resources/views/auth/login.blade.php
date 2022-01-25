9f2f6607d24faf446782f0f9ccef71101aa698fc
lumlop <vincent.dusanek@gmail.com>
2022-01-25 10:56:28 +0100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,24 +1,41 @@
 <x-guest-layout>
+
     <x-auth-card>
 
         <!-- Session Status -->
+
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Validation Errors -->
+        <!-- Session Status -->
+
+        <!-- Validation Fehler -->
+
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
+        <!-- Validation Fehler -->
+
+        <!-- Form -->
+
         <form method="POST" action="{{ route('login') }}">
+
             @csrf
 
-            <!-- Email Address -->
+            <!-- Email Addresse -->
+
             <div>
+
                 <x-label for="email" :value="__('E-Mail-Adresse')" class="mb-2"/>
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+
             </div>
 
-            <!-- Password -->
+            <!-- Email Addresse -->
+
+            <!-- Passwort -->
+
             <div class="mt-4">
+
                 <x-label for="password" :value="__('Passwort')" class="mb-2"/>
 
                 <x-input id="password" class="block mt-1 w-full"
@@ -27,25 +44,52 @@
                                 required autocomplete="current-password" />
             </div>
 
-            <!-- Remember Me -->
+            <!-- Passwort -->
+
+            <!-- Angemeldet bleiben-->
+
             <div class="block mt-4">
+
                 <label for="remember_me" class="inline-flex items-center">
+
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+
                     <span class="ml-2 text-sm text-gray-600">{{ __('Angemeldet bleiben') }}</span>
+
                 </label>
+
             </div>
 
+            <!-- Angemeldet bleiben-->
+
+            <!-- Passwort vergessen? -->
+
             <div class="flex items-center justify-end mt-4">
+
                 @if (Route::has('password.request'))
+
                     <a class="hover:underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+
                         {{ __('Passwort vergessen?') }}
+
                     </a>
+
                 @endif
 
                 <x-button class="ml-3 ">
+
                     {{ __('Anmelden') }}
+
                 </x-button>
+
             </div>
+
+            <!-- Passwort vergessen? -->
+
         </form>
+
+        <!-- Form -->
+
     </x-auth-card>
+
 </x-guest-layout>

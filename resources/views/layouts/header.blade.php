acec00396a4309ad1b2817ef57f97e56c3389a4a
lumlop <vincent.dusanek@gmail.com>
2022-02-17 14:42:11 +0100

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -61,8 +61,36 @@
 	<ul class="flex items-center">
 
 		@auth
+
+		@php
+			$id = auth()->user()->id;
+			if(isset(Auth::user()->image->filename)) {
+				
+			}
+		@endphp
+
+
+			<li class="mx-2">
+
+                    <div class="px-3">
+
+	                    @if(isset(Auth::user()->image->filename)) 
+
+	                    	<a href="{{ route('profile.details', ['id' => $user->id]) }}" class="text-gray-500 hover:text-gray-800 hover:bg-opacity-10 rounded-md flex items-center">
+
+	                    		<img src="{{ url('images/show/'.$id) }}" class="w-10 h-10 rounded-full object-cover border-gray-200">
+
+								<span class="text-xs sm:text-sm font-medium font-semibold text-left px-4">{{ Auth::user()->vorname }} {{ Auth::user()->nachname }}</span>
+
+	                    	</a>
+
+	                    @endif
+
+                    </div>
+
+			</li>
 		
-			<li class="text-gray-700 hover:bg-gray-500 hover:bg-opacity-10 rounded-md">
+			<li class="text-gray-700 hover:bg-gray-500 hover:bg-opacity-10 rounded-md ml-4">
 
 				<form action="{{ route('logout') }}" method="post">
 
@@ -74,7 +102,8 @@
 
 					  		<path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd" />
 
-						</svg>Ausloggen
+						</svg>
+
 
 					</button>
 

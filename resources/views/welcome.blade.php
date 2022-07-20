abb8dc1930280a792347f67f634dc4a5fa3a5979
lumlop <vincent.dusanek@gmail.com>
2022-07-20 11:18:50 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,7 +90,7 @@
 
                         <div class="text-base text-center md:text-left lg:text-left xl:text-left 2xl:text-left sm:max-w-xl sm:mx-auto lg:mx-0">
 
-                            <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                            <h1 class="text-2xl sm:text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
 
                                 <span class="block xl:inline">{{ Config::get('site_vars.welcomeString1') }}</span>
 
@@ -132,7 +132,7 @@
 
                             </div>
 
-                            <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-sm sm:max-w-xl sm:mx-auto md:mt-5 md:text-sm lg:mx-0">
+                            <p class="mt-5 text-gray-500 sm:max-w-xl sm:mx-auto text-sm">
 
                             Informationen und FAQ zur Lehr:werkstatt an der Universität Augsburg finden Sie 
 

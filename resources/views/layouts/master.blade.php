4c25e7f35238f0736846cae661ba3f7b3755a8bb
lumlop <vincent.dusanek@gmail.com>
2022-07-20 14:56:32 +0200

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ Config::get('site_vars.platformName1') }}{{ Config::get('site_vars.platformName2') }} - {{ Config::get('site_vars.welcomeString1') }} {{ Config::get('site_vars.welcomeString2') }}</title>
-        
         <link rel="icon" href="{{ url('img/favicon.png') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 
@@ -20,5 +19,5 @@
         @include('layouts.footer')
 
     </body>
- <!-- rgba(17, 24, 39, 0.9) -->
+
 </html>

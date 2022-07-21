874d5c4566a2c874eea52b25e343e75bc28991f4
lumlop <vincent.dusanek@gmail.com>
2022-07-21 07:55:37 +0200

    Darstellung Nutzername

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -99,7 +99,19 @@
 
                     		@endif
 
-							<span class="text-xs sm:text-sm font-medium font-semibold text-left px-4 break-words">Konto <!--{{ Auth::user()->vorname }} {{ Auth::user()->nachname }} --></span>
+                    		<span class="text-xs sm:text-sm font-medium font-semibold text-left px-4 break-words">
+
+                    		@if (Auth::user()->vorname)
+
+                    			{{ Auth::user()->vorname}} {{ Auth::user()->nachname }}
+
+                    		@else
+
+								Konto <!--{{ Auth::user()->vorname }} {{ Auth::user()->nachname }} -->
+                    		
+                    		@endif
+
+                    		</span>
 
                     	</a>
 

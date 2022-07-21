537198698da2962b429f9f80ec1e599ddac35c97
lumlop <vincent.dusanek@gmail.com>
2022-07-21 09:25:39 +0200

    Update account.blade.php

--- resources/views/profile/account.blade.php
+++ resources/views/profile/account.blade.php
@@ -60,17 +60,27 @@ $valGruesse = Config::get('site_vars.meinBereichGruessePlaceholder');
 
         @include('layouts.navigation')
 
-        <div class="px-1 md:px-8 py-1 md:py-8 text-gray-700 w-screen sm:rounded-r-lg bg-gray-600">
+        <!-- Nav -->
 
-            <div class="bg-white shadow overflow-hidden sm:rounded-lg mb-4">
+        <!-- Inhalt -->
 
-                <div class="px-4 py-5 sm:px-6">
+        <div class="px-1 md:px-8 py-8 md:py-8 text-gray-700 w-screen sm:rounded-r-lg bg-gray-600">
 
-                    <h2 class="text-lg leading-6 font-medium text-gray-900">
+            <div class="overflow-hidden mb-4">
+
+                <div class="mb-4">
+
+                    <h2 class="font-semibold text-lg text-gray-200 grid justify-items-center sm:justify-items-start select-none">
 
                         Accountdaten bearbeiten
 
                     </h2>
+
+                    <div class="mt-1 text-sm text-gray-300 grid text-center sm:text-left flex">
+
+                        Erhalten Sie Einblicke in die Nutzungsstatistiken des Portals. Momentane Statistiken sind hinsichtlich der Registrierungen, Abmeldungen, Nutzenden, Zuweisungen, Studiengänge und des Betreuungsverhältnisses einsehbar. Kontaktieren Sie bei technischen Anregungen und Anliegen das DigiLLab der Universität Augsburg.
+
+                    </div>
 
                 </div>
 
@@ -171,64 +181,64 @@ $valGruesse = Config::get('site_vars.meinBereichGruessePlaceholder');
 
                             <dl>
 
-                                <div class="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <div class="py-5 sm:grid sm:grid-cols-3 sm:gap-4">
 
-                                    <dt class="text-sm font-medium text-gray-500 py-2">
+                                    <dt class="text-sm font-medium text-gray-200 py-2">
 
                                         Bearbeiten Sie die <strong>E-Mail-Adresse</strong>
 
                                     </dt>
 
-                                    <dd class="mt-1 text-sm text-gray-500 text-white hover:text-gray-900 active:text-gray-1000 sm:mt-0 sm:col-span-2 flex items-center border-b-2">
+                                    <dd class="bg-gray-700 mt-1 text-sm text-gray-200 text-white hover:text-gray-400 active:text-gray-1000 sm:mt-0 sm:col-span-2 flex items-center border-b-2 pl-3 transition ease-in-out duration-500">
 
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                             <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
                                             <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
                                         </svg>
 
-                                        {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'w-full px-2 py-2 ml-2 border-b-2 border-gray-200 focus:outline-none focus:text-gray-900 transition ease-in-out duration-500')) !!}
+                                        {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'bg-gray-700 w-full px-2 py-2 ml-2 focus:outline-none focus:text-gray-400')) !!}
 
                                     </dd>
 
                                 </div>
 
-                                <div class="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <div class="py-5 sm:grid sm:grid-cols-3 sm:gap-4">
 
-                                    <dt class="text-sm font-medium text-gray-500 py-2">
+                                    <dt class="text-sm font-medium text-gray-200 py-2">
 
                                         Vergeben Sie ein neues <strong>Passwort</strong>
 
                                     </dt>
 
-                                    <dd class="mt-1 text-sm text-gray-500 text-white hover:text-gray-900 active:text-gray-1000 sm:mt-0 sm:col-span-2 flex items-center border-b-2">
+                                    <dd class="bg-gray-700 mt-1 text-sm text-gray-200 text-white hover:text-gray-400 active:text-gray-1000 sm:mt-0 sm:col-span-2 flex items-center border-b-2 pl-3 transition ease-in-out duration-500">
 
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                             <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
                                             <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
                                         </svg>
 
-                                        {!! Form::password('password', array('placeholder' => 'Passwort','class' => 'w-full px-2 py-2 ml-2 border-b-2 border-gray-200 focus:outline-none focus:text-gray-900 transition ease-in-out duration-500')) !!}
+                                        {!! Form::password('password', array('placeholder' => 'Passwort','class' => 'bg-gray-700 w-full px-2 py-2 ml-2 focus:outline-none focus:text-gray-400')) !!}
 
                                     </dd>
 
                                 </div>
 
-                                <div class="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 mb-6">
+                                <div class="py-5 sm:grid sm:grid-cols-3 sm:gap-4 mb-6">
 
-                                    <dt class="text-sm font-medium text-gray-500 py-2">
+                                    <dt class="text-sm font-medium text-gray-200 py-2">
 
                                         <strong>Bestätigen</strong> Sie das <strong>neue Passwort</strong>
 
                                     </dt>
 
-                                    <dd class="mt-1 text-sm text-gray-500 text-white hover:text-gray-900 active:text-gray-1000 sm:mt-0 sm:col-span-2 flex items-center border-b-2">
+                                    <dd class="bg-gray-700 mt-1 text-sm text-gray-200 text-white hover:text-gray-400 active:text-gray-1000 sm:mt-0 sm:col-span-2 flex items-center border-b-2 pl-3 transition ease-in-out duration-500">
 
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                             <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
                                             <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
                                         </svg>
 
-                                        {!! Form::password('confirm-password', array('placeholder' => 'Passwort bestätigen','class' => 'w-full px-2 py-2 ml-2 border-b-2 border-gray-200 focus:outline-none focus:text-gray-900 transition ease-in-out duration-500')) !!}
+                                        {!! Form::password('confirm-password', array('placeholder' => 'Passwort bestätigen','class' => 'bg-gray-700 w-full px-2 py-2 ml-2 focus:outline-none focus:text-gray-400')) !!}
 
                                     </dd>
 
@@ -248,7 +258,7 @@ $valGruesse = Config::get('site_vars.meinBereichGruessePlaceholder');
 
                         <div class="mb-4 mt-4 mx-4 float-left">
 
-                            <a href="{{ route('profile.edit') }}" class="bg-transparent hover:bg-purple-600 text-purple-600 font-semibold text-sm hover:text-white focus:outline-none focus:ring ring-purple-300 focus:border-purple-300 py-2 pr-4 pl-3 border border-purple-600 hover:border-transparent rounded flex items-center transition ease-in-out duration-150">
+                            <a href="{{ route('profile.edit') }}" class="bg-gray-700 bg-transparent hover:bg-yellow-600 text-white font-semibold text-sm hover:text-white py-2 pr-4 pl-3 border border-gray-700 hover:border-transparent focus:outline-none focus:ring ring-yellow-300 focus:border-yellow-300 rounded flex items-center transition ease-in-out duration-150">
 
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                     <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
@@ -262,7 +272,7 @@ $valGruesse = Config::get('site_vars.meinBereichGruessePlaceholder');
 
                         <div class="mb-4 mt-4 mx-4 float-right">
 
-                            <button type="submit" class="bg-transparent hover:bg-green-600 text-green-600 font-semibold text-sm hover:text-white py-2 pr-4 pl-3 border border-green-600 hover:border-transparent focus:outline-none focus:ring ring-green-300 focus:border-green-300 rounded flex items-center transition ease-in-out duration-150">
+                            <button type="submit" class="bg-gray-700 bg-transparent hover:bg-green-600 text-white font-semibold text-sm hover:text-white py-2 pr-4 pl-3 border border-gray-700 hover:border-transparent focus:outline-none focus:ring ring-green-300 focus:border-green-300 rounded flex items-center transition ease-in-out duration-150">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" viewBox="0 0 20 20" fill="currentColor">
 
                                     <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />

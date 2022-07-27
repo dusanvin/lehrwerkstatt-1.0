2a990d1bf8c2557b5bdc7e7194dfca99c602d147
lumlop <vincent.dusanek@gmail.com>
2022-07-27 12:03:06 +0200

    Zwischenstand II matchings

--- resources/views/matchings.blade.php
+++ resources/views/matchings.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+
 <div class="flex flex-row h-full mx-0 sm:mx-5 mt-1 sm:mt-10 mb-1 sm:mb-10">
 
     <!-- Nav -->
@@ -420,7 +422,7 @@
 
                                 </div>
 
-                                <div class="hidden sm:table-cell px-6 py-4 whitespace-no-wrap">
+                                <div class="hidden sm:table-cell px-6 py-4 whitespace-no-wrap w-96">
 
                                     <div class="text-xs sm:text-sm leading-5 font-medium text-white">
                 
@@ -437,6 +439,90 @@
                                 </div>
 
                                 <div>
+
+                                    
+
+<div x-data="{ modelOpen: false }">
+    <button @click="modelOpen =!modelOpen" class="flex items-center justify-center px-3 py-2 space-x-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-md dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" />
+        </svg>
+
+        <span>Invite Member</span>
+    </button>
+
+    <div x-show="modelOpen" class="fixed inset-0 z-50 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div class="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
+            <div x-cloak @click="modelOpen = false" x-show="modelOpen" 
+                x-transition:enter="transition ease-out duration-300 transform"
+                x-transition:enter-start="opacity-0" 
+                x-transition:enter-end="opacity-100"
+                x-transition:leave="transition ease-in duration-200 transform"
+                x-transition:leave-start="opacity-100" 
+                x-transition:leave-end="opacity-0"
+                class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40" aria-hidden="true"
+            ></div>
+
+            <div x-cloak x-show="modelOpen" 
+                x-transition:enter="transition ease-out duration-300 transform"
+                x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" 
+                x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+                x-transition:leave="transition ease-in duration-200 transform"
+                x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" 
+                x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                class="bg-gray-700 inline-block w-full max-w-xl p-8 my-20 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl 2xl:max-w-2xl"
+            >
+                <div class="flex items-center justify-between space-x-4">
+                    <h1 class="text-xl font-medium text-gray-100">Vorschlag übernehmen</h1>
+
+                    <button @click="modelOpen = false" class="text-gray-600 focus:outline-none hover:text-gray-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    </button>
+                </div>
+
+                <p class="mt-2 text-sm text-gray-400">
+                    Nehmen Sie aufgrund der nachfolgenden Übereinstimmungen den Vorschlag in die übernommenen Vorschläge auf.
+                </p>
+
+                <form class="mt-5">
+                    
+                    
+                    <div class="mt-4">
+                        <h1 class="text-xs font-medium text-gray-400 uppercase">Permissions</h1>
+
+                        <div class="mt-4 space-y-5">
+                            <div class="flex items-center space-x-3 cursor-pointer" x-data="{ show: true }" @click="show =!show">
+                                
+
+                                <p class="text-gray-500">Can make task</p>
+                            </div>
+
+                            <div class="flex items-center space-x-3 cursor-pointer" x-data="{ show: false }" @click="show =!show">
+                                
+
+                                <p class="text-gray-500">Can delete task</p>
+                            </div>
+
+                            <div class="flex items-center space-x-3 cursor-pointer" x-data="{ show: true }" @click="show =!show">
+                                
+
+                                <p class="text-gray-500">Can edit task</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="flex justify-end mt-6">
+                        <button type="button" class="px-3 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-indigo-500 rounded-md dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:bg-indigo-700 hover:bg-indigo-600 focus:outline-none focus:bg-indigo-500 focus:ring focus:ring-indigo-300 focus:ring-opacity-50">
+                            Aufnehmen
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
                                     
 
                                 </div>

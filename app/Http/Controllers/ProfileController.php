2ddf4eb5884ba6fc5d86a2ca5fe4f480aa6a0e37
nsinf <norman.szabo@student.uni-augsburg.de>
2022-08-02 00:24:26 +0200

    bugfix

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -85,14 +85,14 @@ class ProfileController extends Controller
         $user = auth()->user();
 
         if($user->survey_data) {
-            if(strcasecmp($user->role, 'lehr') || strcasecmp($user->role, 'stud')) {
+            if(strcasecmp($user->role, 'lehr') == 0 || strcasecmp($user->role, 'stud') == 0) {
                 $attention = 'Die Bewerbung für den aktuellen Jahrgang 2022/2023 liegt uns vor. Sie können Angaben korrigieren, während Sie das Formular durchgehen. Bitte beachten Sie, dass Pflichtfelder weiterhin ausgefüllt sein und Änderungen anschließend bestätigt werden müssen, bevor diese wirksam werden können.';
                 return view('surveys.'.$user->role, ['attention' => $attention, 'user' => $user]);
             } else {
                 return view('surveys.admin_mod', ['attention' => 'Hier können Sie Ihre Daten korrigieren.', 'user' => $user]);
             }
         } else {
-            if(strcasecmp($user->role, 'lehr') || strcasecmp($user->role, 'stud')) {
+            if(strcasecmp($user->role, 'lehr') == 0 || strcasecmp($user->role, 'stud') == 0) {
                 $attention = 'Zum aktuellen Jahrgang 2022/2023 liegt uns keine Bewerbung vor. Wir bitten Sie, sich kurz Zeit zu nehmen und das Bewerbungsformular auszufüllen.';
                 return view('surveys.'.$user->role, ['attention' => $attention]);
             } else {

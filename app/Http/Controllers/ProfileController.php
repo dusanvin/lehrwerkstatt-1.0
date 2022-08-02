12e1100de913ec041fb73857356e7e085b26cc1e
nsinf <norman.szabo@student.uni-augsburg.de>
2022-08-03 01:20:36 +0200

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -57,18 +57,18 @@ class ProfileController extends Controller
     {
         $user = User::find($id);
         $user->survey_data = json_decode($user->survey_data);
-        if(isset($user->survey_data->faecher))
+        if (isset($user->survey_data->faecher))
             $user->survey_data->faecher = implode(', ', $user->survey_data->faecher);
-        if(isset($user->survey_data->landkreise))
+        if (isset($user->survey_data->landkreise))
             $user->survey_data->landkreise = implode(', ', $user->survey_data->landkreise);
-        if(isset($user->survey_data->verkehrsmittel))
+        if (isset($user->survey_data->verkehrsmittel))
             $user->survey_data->verkehrsmittel = implode(', ', $user->survey_data->verkehrsmittel);
-        if(isset($user->survey_data->praktika))
+        if (isset($user->survey_data->praktika))
             $user->survey_data->praktika = implode(', ', $user->survey_data->praktika);
-        if(isset($user->survey_data->freue_auf))
+        if (isset($user->survey_data->freue_auf))
             $user->survey_data->freue_auf = implode(', ', $user->survey_data->freue_auf);
-        if(!$user->survey_data) {
-            $this->edit(); 
+        if (!$user->survey_data) {
+            $this->edit();
         } else {
             return view('profile.details', compact('user'));
         }
@@ -84,29 +84,30 @@ class ProfileController extends Controller
     {
         $user = auth()->user();
 
-        if($user->survey_data) {
-            if(strcasecmp($user->role, 'lehr') == 0 || strcasecmp($user->role, 'stud') == 0) {
+        if ($user->survey_data) {
+            if (strcasecmp($user->role, 'lehr') == 0 || strcasecmp($user->role, 'stud') == 0) {
                 $attention = 'Die Bewerbung für den aktuellen Jahrgang 2022/2023 liegt uns vor. Sie können Angaben korrigieren, während Sie das Formular durchgehen. Bitte beachten Sie, dass Pflichtfelder weiterhin ausgefüllt sein und Änderungen anschließend bestätigt werden müssen, bevor diese wirksam werden können.';
-                return view('surveys.'.lcfirst($user->role), ['attention' => $attention, 'user' => $user]);
+                return view('surveys.' . lcfirst($user->role), ['attention' => $attention, 'user' => $user]);
             } else {
                 return view('surveys.admin_mod', ['attention' => 'Hier können Sie Ihre Daten korrigieren.', 'user' => $user]);
             }
         } else {
-            if(strcasecmp($user->role, 'lehr') == 0 || strcasecmp($user->role, 'stud') == 0) {
+            if (strcasecmp($user->role, 'lehr') == 0 || strcasecmp($user->role, 'stud') == 0) {
                 $attention = 'Zum aktuellen Jahrgang 2022/2023 liegt uns keine Bewerbung vor. Wir bitten Sie, sich kurz Zeit zu nehmen und das Bewerbungsformular auszufüllen.';
-                return view('surveys.'.lcfirst($user->role), ['attention' => $attention]);
+                return view('surveys.' . lcfirst($user->role), ['attention' => $attention]);
             } else {
                 return view('surveys.admin_mod', ['attention' => 'Bitte vervollständigen Sie die Daten.', 'user' => $user]);
             }
         }
-
     }
 
-    public function account() {
+    public function account()
+    {
         return view('profile.account', ['user' => auth()->user()]);
     }
 
-    public function update(Request $request, $id) {
+    public function update(Request $request, $id)
+    {
         $id = auth()->id();
         $user = User::find($id);
 
@@ -117,19 +118,20 @@ class ProfileController extends Controller
 
         $input = $request->all();
 
-        if($input['email'] != $user->email) {
+        if ($input['email'] != $user->email) {
             $user->email_verified_at = null;
             $user->sendEmailVerificationNotification();
         }
 
         if (isset($input['password'])) {
             $this->validate($request, [
-                'password' => [Password::min(10)
-				->numbers()
-				->symbols()
-				->mixedCase()
-				->letters(),
-			    ]
+                'password' => [
+                    Password::min(10)
+                        ->numbers()
+                        ->symbols()
+                        ->mixedCase()
+                        ->letters(),
+                ]
             ]);
             $input['password'] = Hash::make($input['password']);
         } else {
@@ -143,23 +145,56 @@ class ProfileController extends Controller
         return redirect()->route('profile.account');
     }
 
-    public function matchings() {
+    public function matchings()
+    {
         $user = auth()->user();
         // dd($user);
         // $matchings = [];
         // if(true) { // soll true sein wenn email versendet
         //     $matchings = $user->matchings;
         // }
-        foreach($user->matchings as $m) {
+        
+        foreach ($user->matchings as $m) {
             $m->survey_data = json_decode($m->survey_data);
-            // dd($m->survey_data);
-            if(isset($m->survey_data->faecher))
+            if (isset($m->survey_data->faecher))
                 $m->survey_data->faecher = implode(', ', $m->survey_data->faecher);
-            if(isset($m->survey_data->landkreise))
+            if (isset($m->survey_data->landkreise))
                 $m->survey_data->landkreise = implode(', ', $m->survey_data->landkreise);
-            if(isset($m->survey_data->verkehrsmittel))
+            if (isset($m->survey_data->verkehrsmittel))
                 $m->survey_data->verkehrsmittel = implode(', ', $m->survey_data->verkehrsmittel);
         }
+        // dd($user);
+        
+        // if (lcfirst($user->role) == 'lehr') {
+        //     foreach ($user->matchings as $m) {
+        //         $m->survey_data = json_decode($m->survey_data);
+        //         if (isset($m->survey_data->faecher))
+        //             $m->survey_data->faecher = implode(', ', $m->survey_data->faecher);
+        //         if (isset($m->survey_data->landkreise))
+        //             $m->survey_data->landkreise = implode(', ', $m->survey_data->landkreise);
+        //         if (isset($m->survey_data->verkehrsmittel))
+        //             $m->survey_data->verkehrsmittel = implode(', ', $m->survey_data->verkehrsmittel);
+        //     }
+        // } elseif(lcfirst($user->role) == 'stud') {
+        //     $matching = DB::table('user_user')->where('matching_id', $user->id)->get();
+        //     $matching_id = $matching[0]->user_id;
+        //     $matching_mse = $matching[0]->mse;
+        //     $matching = User::where('id', $matching_id)->get();
+        //     // dd($user);
+        //     $user->matchings = $matching;
+        //     // dd($user);
+        //     foreach ($user->matchings as $m) {
+        //         // dd($m);
+        //         $m->survey_data = json_decode($m->survey_data);
+        //         if (isset($m->survey_data->faecher))
+        //             $m->survey_data->faecher = implode(', ', $m->survey_data->faecher);
+        //         if (isset($m->survey_data->landkreise))
+        //             $m->survey_data->landkreise = implode(', ', $m->survey_data->landkreise);
+        //         if (isset($m->survey_data->verkehrsmittel))
+        //             $m->survey_data->verkehrsmittel = implode(', ', $m->survey_data->verkehrsmittel);
+        //     }
+        // }
+
         // dd($user);
         // dd($matchings);
         return view('profile.matchings', compact('user'));
@@ -195,11 +230,11 @@ class ProfileController extends Controller
     //     if (isset($input['password'])) {
     //         $this->validate($request, [
     //             'password' => [Password::min(10)
-	// 			->numbers()
-	// 			->symbols()
-	// 			->mixedCase()
-	// 			->letters(),
-	// 		    ]
+    // 			->numbers()
+    // 			->symbols()
+    // 			->mixedCase()
+    // 			->letters(),
+    // 		    ]
     //         ]);
     //         $input['password'] = Hash::make($input['password']);
     //     } else {

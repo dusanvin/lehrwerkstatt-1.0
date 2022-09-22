b1bec9d1f3892a633b0e4f60d8c74ec05a26eb29
dusanvin <vincent.dusanek@zlbib.uni-augsburg.de>
2022-09-22 09:17:59 +0200

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -95,34 +95,6 @@
 
 				<!-- Bewerbungsformular -->
 
-				<!-- Mitgliedsprofil -->
-
-				<li class="ml-2 mr-2 my-1 rounded-l-lg rounded-r-lg">
-
-					<a href="{{ route('profile.details', ['id' => auth()->user()->id]) }}" class="text-gray-100 hover:text-gray-400 px-4 py-2 flex items-center rounded-l-md rounded-r-md transition ease-in-out duration-150 @if (Request::is('profile/details/*')) { text-yellow-400 } @endif">
-
-						<div>
-
-							<svg xmlns="http://www.w3.org/2000/svg" class="fill-current h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-
-								<path fill-rule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clip-rule="evenodd" />
-
-							</svg>
-
-						</div>
-
-						<div class="pl-3">
-							<p class="navigation-element text-sm font-medium mb-1 font-semibold">Mitgliedsprofil</p>
-							<!--<p class="navigation-element text-xs">Jahrgang 2022/2023</p>-->
-
-						</div>
-
-					</a>
-
-				</li>
-
-				<!-- Mitgliedsprofil -->
-
 				<!-- Account -->
 
 				<li class="ml-2 mr-2 my-1 rounded-l-lg rounded-r-lg">

0a3a882ef82cca4217ea54ba9a3b206cc8107d82
dusanvin <vincent.dusanek@zlbib.uni-augsburg.de>
2022-10-05 14:39:21 +0200

    roles im Admin-Bereich wieder zum Laufen bringen

--- resources/views/layouts/admin_navigation_users.blade.php
+++ resources/views/layouts/admin_navigation_users.blade.php
@@ -4,12 +4,13 @@
 
         <li class="px-4 py-2 -mb-px font-medium text-xs sm:text-sm text-gray-800 border-b-2 border-gray-700 rounded-t opacity-50 bg-white border-b-4 -mb-px opacity-100"><a href="{{ route('users.index') }}">Personen verwalten</a></li>
 
+        <!-- 
         @role('Admin')
 
             <li class="px-4 py-2 font-medium text-xs sm:text-sm text-gray-800 rounded-t opacity-50 bg-white border-gray-400"><a href="{{ route('roles.index') }}">Rollen verwalten</a></li>
 
         @endrole
-
+		-->
     </ul>
 
 @endrole

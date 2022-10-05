29e70ad8f7a83a46fc6fe4dbbabc2f1c812bddc9
dusanvin <vincent.dusanek@zlbib.uni-augsburg.de>
2022-10-05 22:56:12 +0200

    Responsive Account

--- resources/views/profile/account.blade.php
+++ resources/views/profile/account.blade.php
@@ -165,8 +165,15 @@ $valGruesse = Config::get('site_vars.meinBereichGruessePlaceholder');
 
                     <div class="mt-1 text-sm text-gray-300 text-center sm:text-left">
 
-                        Bearbeiten Sie Ihre Accountdaten. Wenden Sie sich bei Fragen zu Ihrem Account an das <a href="mailto:team@digillab.uni-augsburg.de" class="hover:underline">DigiLLab der Universität Augsburg</a>.
+                        Bearbeiten Sie Ihre Accountdaten. Wenden Sie sich bei Fragen zu Ihrem Account an das 
 
+                            <a href="mailto:team@digillab.uni-augsburg.de" class="hover:underline text-yellow-500">
+
+                                per Mail an das DigiLLab
+                            </a>
+
+                            der Universität Augsburg.
+                            
                     </div>
 
                 </div>
@@ -199,7 +206,7 @@ $valGruesse = Config::get('site_vars.meinBereichGruessePlaceholder');
 
                                     <div class="text-white flex items-center">
 
-                                        <img src="{{ url('images/show/'.$user->id) }}" class="w-28 rounded-md object-cover border-gray-200">
+                                        <img src="{{ url('images/show/'.$user->id) }}" class="w-28 h-28 rounded-md object-cover border-gray-200">
                                         
                                         <span class="text-xs sm:text-sm font-semibold text-left px-4 break-words">
 
@@ -241,9 +248,9 @@ $valGruesse = Config::get('site_vars.meinBereichGruessePlaceholder');
 
                                                 @endif
 
-                                                <div class="flex mt-4">
+                                                <div class="flex mt-4 flex-wrap ">
 
-                                                    <form method="POST" action="{{ route('images.store') }}" enctype="multipart/form-data" class="flex items-center">
+                                                    <form method="POST" action="{{ route('images.store') }}" enctype="multipart/form-data" class="flex items-center mb-2">
 
                                                         @csrf
 
@@ -255,7 +262,7 @@ $valGruesse = Config::get('site_vars.meinBereichGruessePlaceholder');
                                                               <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
                                                             </svg>
 
-                                                            <span class="pl-3 font-semibold">Hochladen</span>
+                                                            <span class="pl-3 font-semibold hidden md:block">Hochladen</span>
 
                                                             @php
                                                             cache()->flush();
@@ -265,7 +272,7 @@ $valGruesse = Config::get('site_vars.meinBereichGruessePlaceholder');
 
                                                     </form>
 
-                                                    <form method="POST" action="{{ route('images.destroy', ['user_id' => $user->id]) }}" class="flex">
+                                                    <form method="POST" action="{{ route('images.destroy', ['user_id' => $user->id]) }}" class="flex mb-2">
 
                                                         @csrf
 
@@ -275,7 +282,7 @@ $valGruesse = Config::get('site_vars.meinBereichGruessePlaceholder');
                                                               <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                             </svg>
 
-                                                            <span class="pl-3 font-semibold">Löschen</span>
+                                                            <span class="pl-3 font-semibold hidden md:block">Löschen</span>
 
                                                             @php
                                                             cache()->flush();
@@ -309,7 +316,7 @@ $valGruesse = Config::get('site_vars.meinBereichGruessePlaceholder');
 
                                     <div class="text-white flex items-center">
 
-                                        <img src="https://daz-buddies.digillab.uni-augsburg.de/img/avatar.jpg" class="w-28 rounded-md object-cover border-gray-200">
+                                        <img src="https://daz-buddies.digillab.uni-augsburg.de/img/avatar.jpg" class="w-28 h-28 rounded-md object-cover border-gray-200">
                                         
                                         <span class="text-xs sm:text-sm font-semibold text-left px-4 break-words">
 

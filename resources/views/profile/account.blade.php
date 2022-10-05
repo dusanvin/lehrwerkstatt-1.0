91102fdae70f134338538d4d6d8f7ca121976cf0
dusanvin <vincent.dusanek@zlbib.uni-augsburg.de>
2022-10-05 22:59:11 +0200

    Update account.blade.php

--- resources/views/profile/account.blade.php
+++ resources/views/profile/account.blade.php
@@ -173,7 +173,7 @@ $valGruesse = Config::get('site_vars.meinBereichGruessePlaceholder');
                             </a>
 
                             der Universität Augsburg.
-                            
+
                     </div>
 
                 </div>
@@ -276,7 +276,7 @@ $valGruesse = Config::get('site_vars.meinBereichGruessePlaceholder');
 
                                                         @csrf
 
-                                                        <button type="submit" class="ml-2 bg-transparent bg-yellow-600 hover:bg-yellow-700 text-white font-semibold text-sm py-2 pr-4 pl-3 border border-yellow-600 hover:border-transparent focus:outline-none focus:ring ring-yellow-600 focus:border-yellow-300 rounded flex items-center transition-colors duration-200 transform duration-150 hover:scale-105">
+                                                        <button type="submit" class="ml-2 bg-transparent bg-red-600 hover:bg-red-700 text-white font-semibold text-sm py-2 pr-4 pl-3 border border-red-600 hover:border-transparent focus:outline-none focus:ring ring-red-600 focus:border-red-300 rounded flex items-center transition-colors duration-200 transform duration-150 hover:scale-105">
 
                                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-1">
                                                               <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
